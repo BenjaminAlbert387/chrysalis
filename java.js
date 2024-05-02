5ecd10282ffad2657d929bff867d64c8c8b78b6e
Benjamin <158299275+BenjaminAlbert387@users.noreply.github.com>
2024-05-02 23:04:14 +0100

    Update java.js

--- java.js
+++ java.js
@@ -8,6 +8,11 @@ function Week9Button()
   window.location.href = "Week9.html"
 }
 
+function Week10Button()
+{
+  window.location.href = "Week10.html"
+}
+
 function ReturnBackFunction()
 {
   window.location.href = "index.html"
