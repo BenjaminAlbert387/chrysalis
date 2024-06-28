fd1ed47dbca168ed39f92ba3ff7c4a3006f587dd
Benjamin <158299275+BenjaminAlbert387@users.noreply.github.com>
2024-06-28 20:38:26 +0100

    Update java.js

--- driving/java.js
+++ driving/java.js
@@ -3,6 +3,11 @@ function ReturnBackFunction()
   window.location.href = "chrysalis/index.html"
 }
 
+function ReturnDrivingFunction()
+{
+  window.location.href = "about.html"
+}
+
 function Hours16Button()
 {
   window.location.href = "16Hours.html"
