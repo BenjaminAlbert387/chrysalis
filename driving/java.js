0b33aee0c4980481795585205fed4e88487c65b3
Benjamin <158299275+BenjaminAlbert387@users.noreply.github.com>
2024-06-28 21:05:45 +0100

    Update java.js

--- driving/java.js
+++ driving/java.js
@@ -12,3 +12,13 @@ function Hours16Button()
 {
   window.location.href = "16Hours.html"
 }
+
+function Hours17Button()
+{
+  window.location.href = "17Hours.html"
+}
+
+function Hours18Button()
+{
+  window.location.href = "18Hours.html"
+}
