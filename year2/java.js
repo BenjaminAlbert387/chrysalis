8ad7301b013683321074b258e7b6f76fcb0e2f9e
Benjamin <158299275+BenjaminAlbert387@users.noreply.github.com>
2024-08-31 17:10:19 +0100

    Create java.js

---
+++ year2/java.js
@@ -0,0 +1,124 @@
+function Y2Week9Button()
+{
+  window.location.href = "Y2Week9.html"
+}
+
+function Y2Week10Button()
+{
+  window.location.href = "Y2Week10.html"
+}
+
+function Y2Week11Button()
+{
+  window.location.href = "Y2Week11.html"
+}
+
+function Y2Week12Button()
+{
+  window.location.href = "Y2Week12.html"
+}
+
+function Y2Week13Button()
+{
+  window.location.href = "Y2Week13.html"
+}
+
+function Y2Week14Button()
+{
+  window.location.href = "Y2Week14.html"
+}
+
+function Y2Week15Button()
+{
+  window.location.href = "Y2Week15.html"
+}
+
+function Y2Week16Button()
+{
+  window.location.href = "Y2Week16.html"
+}
+
+function Y2Week17Button()
+{
+  window.location.href = "Y2Week17.html"
+}
+
+function Y2Week18Button()
+{
+  window.location.href = "Y2Week18.html"
+}
+
+function Y2Week19Button()
+{
+  window.location.href = "Y2Week19.html"
+}
+
+function Y2Week20Button()
+{
+  window.location.href = "Y2Week20.html"
+}
+
+function Y2Week27Button()
+{
+  window.location.href = "Y2Week27.html"
+}
+
+function Y2Week28Button()
+{
+  window.location.href = "Y2Week28.html"
+}
+
+function Y2Week29Button()
+{
+  window.location.href = "Y2Week29.html"
+}
+
+function Y2Week30Button()
+{
+  window.location.href = "Y2Week30.html"
+}
+
+function Y2Week31Button()
+{
+  window.location.href = "Y2Week31.html"
+}
+
+function Y2Week32Button()
+{
+  window.location.href = "Y2Week32.html"
+}
+
+function Y2Week33Button()
+{
+  window.location.href = "Y2Week33.html"
+}
+
+function Y2Week34Button()
+{
+  window.location.href = "Y2Week34.html"
+}
+
+function Y2Week35Button()
+{
+  window.location.href = "Y2Week35.html"
+}
+
+function Y2Week38Button()
+{
+  window.location.href = "Y2Week38.html"
+}
+
+function Y2Week39Button()
+{
+  window.location.href = "Y2Week39.html"
+}
+
+function Y2Week40Button()
+{
+  window.location.href = "Y2Week40.html"
+}
+
+function ReturnBackFunction()
+{
+  window.location.href = "../index.html"
+}
